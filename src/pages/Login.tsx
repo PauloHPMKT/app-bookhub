19f4576800f116ea683627ca19fc1740efa3e9ae
Paulo Sergio <pauloserg861@gmail.com>
2024-11-02 13:52:22 -0300

    refactor: add BaseInput to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,7 @@
-// import { IoEyeOutline } from "react-icons/io5";
-// import { IoEyeOffOutline } from "react-icons/io5";
-
 import { FormEvent } from 'react'
-import logo from '../assets/img/logo-bookhub.png'
 import { MainButton } from '../components/MainButton'
+import { BaseInput } from '../components/BaseInput'
+import logo from '../assets/img/logo-bookhub.png'
 
 export const Login = () => {
   const handleSubmit = (e: FormEvent) => {
@@ -13,11 +11,11 @@ export const Login = () => {
   const teste = () => {
     alert('Teste')
   }
-  
+
   return (
     <div className="flex w-full h-screen">
       <div className="w-[70%] bg-[url('src/assets/img/banner-login.jpg')] bg-cover bg-no-repeat"></div>
-      <div className="w-[500px] flex mt-36"> 
+      <div className="w-[30%] flex mt-36"> 
         <div className="flex flex-col items-center w-full">
           <img 
             src={logo} 
@@ -26,8 +24,20 @@ export const Login = () => {
           />
           <form onSubmit={handleSubmit} className="w-[60%] mt-20">
             <div className='flex flex-col gap-5'>
-              <input type="text" placeholder='Email' className="border-2 border-primary h-10 rounded-lg px-2" />
-              <input type="text" placeholder='Senha' className="border-2 border-primary h-10 rounded-lg px-2" />
+              <BaseInput 
+                type='email'
+                placeholder="Email"
+              />
+              <BaseInput 
+                type='password'
+                placeholder="Senha"
+              />
+              <div className="flex items-center">
+                <input type="checkbox" className="mr-2"/>
+                <label htmlFor="connected" className="text-gray-500">
+                  Manter conectado
+                </label>
+              </div>
               <div className='text-center'>
                 <MainButton type="submit" onClick={teste}>
                   Login
