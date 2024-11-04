5df902440a394afbda6d4a2b04f93ea4bb245bd2
Paulo Sergio <pauloserg861@gmail.com>
2024-11-04 08:38:09 -0300

    feat: add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,34 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { Login } from "../pages/Login";
+import { PageDefault } from "../Layout/PageDefault";
+import { Dashboard } from "../pages/bookhub-pages/Dashboard";
+import { Employees } from "../pages/bookhub-pages/Employees";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <Navigate to="/login" />
   },
   {
     path: "/login",
     element: <Login />
+  },
+  {
+    path: "/app",
+    element: <PageDefault />,
+    children: [
+      {
+        path: "/app",
+        element: <Navigate to="/app/dashboard" />
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />
+      },
+      {
+        path: "employees",
+        element: <Employees />
+      }
+    ]
   }
 ])
